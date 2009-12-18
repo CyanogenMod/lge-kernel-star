5648a3bbc09b7c4b404cbf52aab5fa7edbaffdf3
Russell King <rmk+kernel@arm.linux.org.uk>
2009-12-18 14:34:43 +0000

    ARM: Convert VFP/Crunch/XscaleCP thread_release() to exit_thread()
    This avoids races in the VFP code where the dead thread may have
    state on another CPU.  By moving this code to exit_thread(), we
    will be running as the thread, and therefore be running on the
    current CPU.
    
    This means that we can ensure that the only local state is accessed
    in the thread notifiers.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Change-Id: I195ea8ce8f5f438166b402411eaff3f33f4b2490
    Reviewed-on: http://git-master/r/2731
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Tested-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Gary King <gking@nvidia.com>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -361,16 +361,17 @@ void show_regs(struct pt_regs * regs)
 	__backtrace();
 }
 
+ATOMIC_NOTIFIER_HEAD(thread_notify_head);
+
+EXPORT_SYMBOL_GPL(thread_notify_head);
+
 /*
  * Free current thread data structures etc..
  */
 void exit_thread(void)
 {
+	thread_notify(THREAD_NOTIFY_EXIT, current_thread_info());
 }
-
-ATOMIC_NOTIFIER_HEAD(thread_notify_head);
-
-EXPORT_SYMBOL_GPL(thread_notify_head);
 
 void flush_thread(void)
 {
@@ -386,9 +387,6 @@ void flush_thread(void)
 
 void release_thread(struct task_struct *dead_task)
 {
-	struct thread_info *thread = task_thread_info(dead_task);
-
-	thread_notify(THREAD_NOTIFY_RELEASE, thread);
 }
 
 asmlinkage void ret_from_fork(void) __asm__("ret_from_fork");
