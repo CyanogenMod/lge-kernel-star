74ca02c6b6f74770e1170d84cbb191e3874315de
Gary King <gking@nvidia.com>
2010-03-04 21:27:18 -0800

    [arm] implement TLS register workaround for Tegra errata 657451
    tegra 2 systems have a hardware errata which causes bit 20 of the
    TLS register (CP15 c13, operations 2-4) to be unreliable.
    
    in common user space threading libraries (glibc pthreads, bionic
    pthreads), the value stored in this register is guaranteed to be
    at least word-aligned, leaving bit 0 free.
    
    the work-around for this hardware errata is storing bit 20 of the
    user space-provided TLS value into bit 0 of the register inside
    __set_tls, and restoring it in the get_tls helper.

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -504,7 +504,13 @@ asmlinkage int arm_syscall(int no, struct pt_regs *regs)
 	case NR(set_tls):
 		thread->tp_value = regs->ARM_r0;
 #if defined(CONFIG_HAS_TLS_REG)
+#if defined(CONFIG_TEGRA_ERRATA_657451)
+		BUG_ON(regs->ARM_r0 & 0x1);
+		asm ("mcr p15, 0, %0, c13, c0, 3" : :
+			"r" ((regs->ARM_r0) | ((regs->ARM_r0>>20) & 0x1)));
+#else
 		asm ("mcr p15, 0, %0, c13, c0, 3" : : "r" (regs->ARM_r0) );
+#endif
 #elif !defined(CONFIG_TLS_REG_EMUL)
 		/*
 		 * User space must never try to access this directly.
