41224d48702abd5bf69b8452eedcde1ec8a2f165
Gary King <gking@nvidia.com>
2010-05-11 20:42:12 -0700

    [ARM/tegra] harmony: add tegra-kbc platform device
    Change-Id: Ibaf645a324695f211421c5082c06afde8a12de38

--- arch/arm/mach-tegra/board-generic.c
+++ arch/arm/mach-tegra/board-generic.c
@@ -31,6 +31,8 @@
 #include <mach/iomap.h>
 #include <mach/irqs.h>
 
+#include <mach/kbc.h>
+
 #include "board.h"
 
 /* NVidia bootloader tags */
@@ -107,9 +109,71 @@ static struct platform_device debug_uart = {
 	},
 };
 
+#ifdef CONFIG_KEYBOARD_TEGRA
+static struct resource tegra_kbc_resources[] = {
+	[0] = {
+		.start = TEGRA_KBC_BASE,
+		.end   = TEGRA_KBC_BASE + TEGRA_KBC_SIZE - 1,
+		.flags = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start = INT_KBC,
+		.end   = INT_KBC,
+		.flags = IORESOURCE_IRQ,
+	},
+};
+#define kbc_row(x)				\
+	{					\
+		.is_row = true,			\
+		.is_col = false,		\
+		.num = (x),			\
+	}
+#define kbc_col(x)				\
+	{					\
+		.is_row = false,		\
+		.is_col = true,			\
+		.num = (x),			\
+	}
+
+
+static struct tegra_kbc_plat harmony_kbc = {
+	.debounce_cnt = 2,
+	.repeat_cnt = 5,
+	.wake_cnt = 0,
+	.pin_cfg = {
+		[0] = kbc_row(0), [1] = kbc_row(1),
+		[2] = kbc_row(2), [3] = kbc_row(3),
+		[4] = kbc_row(4), [5] = kbc_row(5),
+		[6] = kbc_row(6), [7] = kbc_row(7),
+		[8] = kbc_row(8), [9] = kbc_row(9),
+		[10] = kbc_row(10), [11] = kbc_row(11),
+		[12] = kbc_row(12), [13] = kbc_row(13),
+		[14] = kbc_row(14), [15] = kbc_row(15),
+		[16] = kbc_col(0), [17] = kbc_col(1),
+		[18] = kbc_col(2), [19] = kbc_col(3),
+		[20] = kbc_col(4), [21] = kbc_col(5),
+		[22] = kbc_col(6), [23] = kbc_col(7),
+	},
+	.keymap = NULL,
+	.wake_cfg = NULL,
+};
+static struct platform_device tegra_kbc = {
+	.name = "tegra-kbc",
+	.id = -1,
+	.dev = {
+		.platform_data = &harmony_kbc,
+	},
+	.resource = tegra_kbc_resources,
+	.num_resources = ARRAY_SIZE(tegra_kbc_resources),
+};
+#endif
+
 static struct platform_device *harmony_devices[] __initdata = {
 #if !defined(CONFIG_TEGRA_DEBUG_UART_NONE)
 	&debug_uart,
+#endif
+#ifdef CONFIG_KEYBOARD_TEGRA
+	&tegra_kbc,
 #endif
 };
 
@@ -122,11 +186,13 @@ static void __init tegra_generic_init(void)
 	clk = clk_get_sys(NULL, "pll_p");
 	clk_enable(clk);
 	clk_set_rate(clk, 216000000);
+	clk_put(clk);
 
 #if !defined(CONFIG_TEGRA_DEBUG_UART_NONE)
 	clk = clk_get_sys(DEBUG_UART_CLK, NULL);
 	clk_set_rate(clk, 216000000);
 	clk_enable(clk);
+	clk_put(clk);
 #endif
 
 	platform_add_devices(harmony_devices, ARRAY_SIZE(harmony_devices));
