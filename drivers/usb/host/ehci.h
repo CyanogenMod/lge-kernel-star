7a7a53f0d4a7f2335a43db69adfdccc58effbbd6
Gary King <gking@nvidia.com>
2010-05-17 11:45:40 -0700

    USB: ehci: add transceiver structure for OTG-compliant HCDs
    some EHCI-compliant host controllers, like Tegra, support dual-mode
    device & host operation; add an otg_transceiver structure to the EHCI
    HCD to support this mode of operation
    
    Change-Id: I6722e036cad06a6eee3bf89b6ba4c88928eafca4

--- drivers/usb/host/ehci.h
+++ drivers/usb/host/ehci.h
@@ -106,6 +106,13 @@ struct ehci_hcd {			/* one per controller */
 	unsigned long		suspended_ports;	/* which ports are
 			suspended */
 
+#ifdef CONFIG_USB_OTG_UTILS
+	/*
+	 * Transceiver decleration for OTG support;
+	 */
+	struct otg_transceiver	*transceiver;
+#endif
+
 	/* per-HC memory pools (could be per-bus, but ...) */
 	struct dma_pool		*qh_pool;	/* qh per active urb */
 	struct dma_pool		*qtd_pool;	/* one or more per qh */
@@ -201,6 +208,9 @@ static void free_cached_itd_list(struct ehci_hcd *ehci);
 /*-------------------------------------------------------------------------*/
 
 #include <linux/usb/ehci_def.h>
+#ifdef CONFIG_USB_OTG_UTILS
+#include <linux/usb/otg.h>
+#endif
 
 /*-------------------------------------------------------------------------*/
 
