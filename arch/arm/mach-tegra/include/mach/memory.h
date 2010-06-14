38d061ef82a022f570f06579472f41fa05bc7b83
Michael Hsu <mhsu@nvidia.com>
2010-06-14 16:49:03 -0700

    [ARM/tegra] Apply 2.6.29 USB network driver fixes to 2.6.32
    Bug 596345
    Fixes USB CDC class modem, IP packet corruption.
    
    Change-Id: Ib703ed1e226d1d659038f3ebb61674e912ec73c4
    Reviewed-on: http://git-master/r/2626
    Reviewed-by: Michael Hsu <mhsu@nvidia.com>
    Tested-by: Michael Hsu <mhsu@nvidia.com>
    Reviewed-by: Gary King <gking@nvidia.com>

--- arch/arm/mach-tegra/include/mach/memory.h
+++ arch/arm/mach-tegra/include/mach/memory.h
@@ -24,5 +24,13 @@
 /* physical offset of RAM */
 #define PHYS_OFFSET		UL(0)
 
+/*
+ * Unaligned DMA causes tegra dma to place data on 4-byte boundary after
+ * expected address. Call to skb_reserve(skb, NET_IP_ALIGN) was causing skb
+ * buffers in usbnet.c to become unaligned.
+ */
+#define NET_IP_ALIGN	0
+#define NET_SKB_PAD	L1_CACHE_BYTES
+
 #endif
 
