83126695d7592eb30810c89f1035fa604712bf75
Bharat Nihalani <bnihalani@nvidia.com>
2010-07-14 11:53:04 +0530

    [odm] harmony: select lp1/lp0 during nvflash
    For LP0, bit 31 of "odmdata" should be set to 1.
    For LP1, bit 31 of "odmdata" should be set to 0.
    
    Change-Id: I9b495c298faec7c38b1493a4c89fe07e6e4625ab
    Reviewed-on: http://git-master/r/3908
    Reviewed-by: Amit Kamath <akamath@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/odm_kit/query/harmony/nvodm_query.c
+++ arch/arm/mach-tegra/odm_kit/query/harmony/nvodm_query.c
@@ -706,7 +706,8 @@ const NvOdmSocPowerStateInfo* NvOdmQueryLowestSocPowerState(void)
             LPStateSelection = NV_DRF_VAL(TEGRA_DEVKIT, BCT_CUSTOPT, LPSTATE, LPStateSelection);
         }
         // Lowest power state controlled by the flashed custom option.
-        PowerStateInfo.LowestPowerState = NvOdmSocPowerState_Suspend;
+        PowerStateInfo.LowestPowerState =  ((LPStateSelection != TEGRA_DEVKIT_BCT_CUSTOPT_0_LPSTATE_LP1)?
+                                            NvOdmSocPowerState_Suspend : NvOdmSocPowerState_DeepSleep);
         pPowerStateInfo = (const NvOdmSocPowerStateInfo*) &PowerStateInfo;
     }
     return (pPowerStateInfo);
