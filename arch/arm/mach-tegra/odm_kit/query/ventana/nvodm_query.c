d8b15af16bce67bcb47eda7938ca12695affc681
Suresh Mangipudi <smangipudi@nvidia.com>
2010-09-02 10:42:19 +0530

    [ventana/rtc] enable rtc as wakeup event.
    enable rtc as a valid wake up source/event on ventana.
    
    Bug 728544
    
    (cherry picked from commit a93c1ee40eddaa6c2244c30f03868fb589fdf062)
    
    Change-Id: Ib1ee0211d3a69a6dbb83f6b78ebc89863c64a499
    Reviewed-on: http://git-master.nvidia.com/r/6778
    Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Tested-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- arch/arm/mach-tegra/odm_kit/query/ventana/nvodm_query.c
+++ arch/arm/mach-tegra/odm_kit/query/ventana/nvodm_query.c
@@ -68,7 +68,7 @@ static NvOdmWakeupPadInfo s_NvOdmWakeupPadInfo[] =
     {NV_FALSE, 13, NvOdmWakeupPadPolarity_Low},     // Wake Event 13 - sdio1_dat1
     {NV_FALSE, 14, NvOdmWakeupPadPolarity_High},    // Wake Event 14 - gp3_pv[6] (WLAN_INT)
     {NV_FALSE, 15, NvOdmWakeupPadPolarity_AnyEdge}, // Wake Event 15 - gmi_ad16  (SPI3_DOUT, DTV_SPI4_CS1)
-    {NV_FALSE, 16, NvOdmWakeupPadPolarity_High},    // Wake Event 16 - rtc_irq
+    {NV_TRUE, 16, NvOdmWakeupPadPolarity_High},    // Wake Event 16 - rtc_irq
     {NV_FALSE,  17, NvOdmWakeupPadPolarity_High},    // Wake Event 17 - kbc_interrupt
     {NV_FALSE, 18, NvOdmWakeupPadPolarity_Low},     // Wake Event 18 - pwr_int (PMIC_INT)
     {NV_FALSE, 19, NvOdmWakeupPadPolarity_High},    // Wake Event 19 - usb_vbus_wakeup[0]
