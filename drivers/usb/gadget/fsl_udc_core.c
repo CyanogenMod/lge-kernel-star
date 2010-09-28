b50b242037add491f8ccc2859c4e0ed3cd7468f6
Rama Kandhala <rkandhala@nvidia.com>
2010-09-28 15:03:03 +0530

    [tegra-udc] Fix device compliance test
    On k32, remote wake up is enabled in the descriptor based on gadget->ops. Since,
    remote wakeup is not supported, disabled gadget->op->remote_wakeup based
    on USB_ANDROID.
    
    Bug 710624
    
    (cherry picked from commit c0340053278c9c9ceb98772cc3566d298dca4d9a)
    
    Change-Id: I760632dd38a9f56dadfbb4d446e83f28cdb5338f
    Reviewed-on: http://git-master/r/7556
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Hanumanth Venkateswa Moganty <vmoganty@nvidia.com>
    Tested-by: Hanumanth Venkateswa Moganty <vmoganty@nvidia.com>

--- drivers/usb/gadget/fsl_udc_core.c
+++ drivers/usb/gadget/fsl_udc_core.c
@@ -1251,7 +1251,9 @@ static int fsl_pullup(struct usb_gadget *gadget, int is_on)
 /* defined in gadget.h */
 static struct usb_gadget_ops fsl_gadget_ops = {
 	.get_frame = fsl_get_frame,
+#ifndef CONFIG_USB_ANDROID
 	.wakeup = fsl_wakeup,
+#endif
 /*	.set_selfpowered = fsl_set_selfpowered,	*/ /* Always selfpowered */
 	.vbus_session = fsl_vbus_session,
 	.vbus_draw = fsl_vbus_draw,
