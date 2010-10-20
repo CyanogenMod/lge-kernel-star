5806e99c3002386ad05bec2fed65d5c36c8bb128
Colin Cross <ccross@android.com>
2010-10-19 21:09:29 -0700

    [ARM] tegra: Add function to copy framebuffer contents
    Due to conflicting restrictions on the location of the framebuffer
    between the bootloader and the protected aperture, the framebuffer
    is likely to need to be moved during boot.  This patch provides
    tegra_move_framebuffer, which can handle move the framebuffer from
    lowmem, highmem, or unmapped memory into unmapped memory.
    
    Change-Id: Ic37e5e337cd3129065fe56fd7777a86d06ad69ac

--- arch/arm/mach-tegra/board.h
+++ arch/arm/mach-tegra/board.h
@@ -34,6 +34,8 @@ void __init tegra_reserve(unsigned long carveout_size, unsigned long fb_size,
 int __init tegra_pcie_init(bool init_port0, bool init_port1);
 void tegra_init_cache(void);
 void __init tegra_protected_aperture_init(unsigned long aperture);
+void tegra_move_framebuffer(unsigned long to, unsigned long from,
+	unsigned long size);
 
 extern unsigned long tegra_bootloader_fb_start;
 extern unsigned long tegra_bootloader_fb_size;
