1ccbe6b140b87713d9dc107dc7c8a64d1e6cd391
Hoang Pham <hopham@nvidia.com>
2010-10-21 19:35:14 -0700

    [tegra/odm_kit]: Update copyright notices
    Update the appropriate licensing header for open source
    Bug 744290
    
    (cherry picked from commit 9680517ed63ab99fbd80fd3f7795d4bc33e34b9c)
    
    Change-Id: Icc2e1dc3f9ec055e6d49dedd3d5ce32b84e3a17a
    Reviewed-on: http://git-master/r/10502
    Tested-by: Hoang Pham <hopham@nvidia.com>
    Reviewed-by: John Davis <jodavis@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/odm_kit/adaptations/misc/ventana/nvodm_sdio.c
+++ arch/arm/mach-tegra/odm_kit/adaptations/misc/ventana/nvodm_sdio.c
@@ -1,11 +1,23 @@
 /*
- * Copyright (c) 2010 NVIDIA Corporation.  All rights reserved.
+ * arch/arm/mach-tegra/odm_kit/adaptions/misc/ventana/nvodm_sdio.c
  *
- * NVIDIA Corporation and its licensors retain all intellectual property
- * and proprietary rights in and to this software, related documentation
- * and any modifications thereto.  Any use, reproduction, disclosure or
- * distribution of this software and related documentation without an express
- * license agreement from NVIDIA Corporation is strictly prohibited.
+ * Implementation of the odm sdio API
+ *
+ * Copyright (c) 2010 NVIDIA Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #include "nvodm_sdio.h"
