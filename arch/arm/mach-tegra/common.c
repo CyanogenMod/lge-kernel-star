03232bf9fbd33bdefa92574c17134203b7f3de1d
Varun Wadekar <vwadekar@nvidia.com>
2010-10-24 16:25:32 +0530

    [ARM] tegra: use dma to read/write fuse registers
    tegra2 hangs if fuse registers are accessed during an apb dma
    operation. war is to use apb dma to read/write fuse registers
    instead.
    
    Change-Id: I4d99a1ad56115c0d73e9cd0679cf38f70f922f3d
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -142,7 +142,9 @@ static void tegra_pm_restart(char mode, const char *cmd)
 void __init tegra_init_early(void)
 {
 	arm_pm_restart = tegra_pm_restart;
-
+#ifdef CONFIG_TEGRA_SYSTEM_DMA
+	tegra_dma_init();
+#endif
 	tegra_init_fuse();
 	tegra_gpio_resume_init();
 	tegra_init_clock();
