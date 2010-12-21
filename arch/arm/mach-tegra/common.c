903fefe0dac3eaff61641864154344e26cb2a34a
Jon Mayo <jmayo@nvidia.com>
2010-12-21 15:09:33 -0800

    [ARM] tegra: use APB DMA for accessing APB devices
    Change-Id: I165411a14342666cbac02fb8cb171580ab0826aa
    Reviewed-on: http://git-master/r/14464
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>
    Tested-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    Signed-off-by: Jon Mayo <jmayo@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -34,6 +34,7 @@
 #include <mach/powergate.h>
 #include <mach/system.h>
 
+#include "apbio.h"
 #include "board.h"
 #include "clock.h"
 #include "fuse.h"
@@ -151,7 +152,7 @@ void __init tegra_init_early(void)
 	tegra_init_power();
 	tegra_init_cache();
 	tegra_dma_init();
-	tegra_init_fuse_dma();
+	tegra_init_apb_dma();
 }
 
 static int __init tegra_lp0_vec_arg(char *options)
