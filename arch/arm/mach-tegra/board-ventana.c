4cabc8d79e1c6ef7cf77889e790ea426634448de
Rakesh Bodla <rbodla@nvidia.com>
2011-01-03 14:46:22 +0530

    [usb/tegra] USB MASS_STORAGE support for ventana
    Adding the support for mass storage feature on ventana.
    
    Bug 773115
    
    Original-Change-Id: Id9fe45ba18ac4661cce730ad1e6cb772e54fec59
    Reviewed-on: http://git-master/r/14777
    Reviewed-by: Rakesh Bodla <rbodla@nvidia.com>
    Tested-by: Rakesh Bodla <rbodla@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board-ventana.c
+++ arch/arm/mach-tegra/board-ventana.c
@@ -58,6 +58,21 @@
 #include "fuse.h"
 #include "wakeups-t2.h"
 
+static struct usb_mass_storage_platform_data tegra_usb_fsg_platform = {
+	.vendor = "NVIDIA",
+	.product = "Tegra 2",
+	.nluns = 1,
+	.bulk_size = 16384,
+};
+
+static struct platform_device tegra_usb_fsg_device = {
+	.name = "usb_mass_storage",
+	.id = -1,
+	.dev = {
+		.platform_data = &tegra_usb_fsg_platform,
+	},
+};
+
 static struct plat_serial8250_port debug_uart_platform_data[] = {
 	{
 		.membase	= IO_ADDRESS(TEGRA_UARTD_BASE),
@@ -150,8 +165,9 @@ static __initdata struct tegra_clk_init_table ventana_clk_init_table[] = {
 	{ NULL,		NULL,		0,		0},
 };
 
-static char *usb_functions[] = { "mtp" };
-static char *usb_functions_adb[] = { "mtp", "adb" };
+static char *usb_functions[] = { "mtp", "usb_mass_storage" };
+static char *usb_functions_adb[] = { "mtp", "adb", "usb_mass_storage" };
+
 
 static struct android_usb_product usb_products[] = {
 	{
@@ -313,6 +329,7 @@ static struct platform_device tegra_camera = {
 };
 
 static struct platform_device *ventana_devices[] __initdata = {
+	&tegra_usb_fsg_device,
 	&androidusb_device,
 	&tegra_uartb_device,
 	&tegra_uartc_device,
