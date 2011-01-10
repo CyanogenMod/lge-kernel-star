90d2a8e857f8a1439b912718d0ea488f5a919135
Scott Williams <scwilliams@nvidia.com>
2011-01-09 17:47:56 -0800

    arm: tegra: ehci: Fix warnings if CONFIG_PM is not defined
    Original-Change-Id: Iaa8cbdf0ac37b295461c9e4accb5c3a15caa0f7f
    Reviewed-on: http://git-master/r/15364
    Tested-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Original-Change-Id: I83c384ccdcc65bcb56f71804faa896997816d44c

--- drivers/usb/host/ehci-tegra.c
+++ drivers/usb/host/ehci-tegra.c
@@ -263,6 +263,7 @@ done:
 	return retval;
 }
 
+#ifdef CONFIG_PM
 static void tegra_ehci_restart(struct usb_hcd *hcd)
 {
 	struct ehci_hcd *ehci = hcd_to_ehci(hcd);
@@ -433,6 +434,7 @@ restart:
 
 	return 0;
 }
+#endif
 
 static void tegra_ehci_shutdown(struct usb_hcd *hcd)
 {
