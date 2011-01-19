52ce853784b6ee9474e80b3be6806dc7c2a6df19
Prashant Gaikwad <pgaikwad@nvidia.com>
2011-01-19 18:01:11 +0530

    i2c: tegra: functionality for byte data
    ADXL34x expects I2C_FUNC_SMBUS_BYTE_DATA functionality.
    
    Bug 780004
    
    Original-Change-Id: I7820050e7b94eec8614bb42bb1a83319a1408203
    Reviewed-on: http://git-master/r/16235
    Tested-by: Prashant Gaikwad <pgaikwad@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -558,7 +558,7 @@ static int tegra_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[],
 
 static u32 tegra_i2c_func(struct i2c_adapter *adap)
 {
-	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_BYTE;
+	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_BYTE | I2C_FUNC_SMBUS_BYTE_DATA;
 }
 
 static const struct i2c_algorithm tegra_i2c_algo = {
