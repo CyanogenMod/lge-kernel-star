06adee65a1962444410f359cf4d2b6a6cd86f0f2
Nitin Kumbhar <nkumbhar@nvidia.com>
2011-02-02 11:59:56 +0530

    arm: tegra_hsuart: Add wake_peer operation
    Recently wake_peer op was added to uart_ops. Add this op for
    tegra_hsuart allowing a platform to implement peer specific wakeup.
    
    BUG 781303
    
    Original-Change-Id: Icfbac324815d7737c0e0820e57a2e8d844855ba0
    Reviewed-on: http://git-master/r/17993
    Tested-by: Nitin Kumbhar <nkumbhar@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/tty/serial/tegra_hsuart.c
+++ drivers/tty/serial/tegra_hsuart.c
@@ -41,6 +41,8 @@
 #include <linux/debugfs.h>
 #include <linux/slab.h>
 #include <linux/workqueue.h>
+#include <linux/tegra_uart.h>
+
 #include <mach/dma.h>
 #include <mach/clk.h>
 
@@ -841,6 +843,14 @@ static void tegra_shutdown(struct uart_port *u)
 	dev_vdbg(u->dev, "-tegra_shutdown\n");
 }
 
+static void tegra_wake_peer(struct uart_port *u)
+{
+	struct tegra_uart_platform_data *pdata = u->dev->platform_data;
+
+	if (pdata && pdata->wake_peer)
+		pdata->wake_peer(u);
+}
+
 static unsigned int tegra_get_mctrl(struct uart_port *u)
 {
 	/* RI - Ring detector is active
@@ -1143,6 +1153,7 @@ static struct uart_ops tegra_uart_ops = {
 	.break_ctl	= tegra_break_ctl,
 	.startup	= tegra_startup,
 	.shutdown	= tegra_shutdown,
+	.wake_peer	= tegra_wake_peer,
 	.set_termios	= tegra_set_termios,
 	.pm		= tegra_pm,
 	.type		= tegra_type,
