b731dba16d137121829b5a3de1903c3b196ca49a
Pavan Kunapuli <pkunapuli@nvidia.com>
2011-02-08 03:08:04 -0800

    ARM: tegra: cardhu:Passing sd power rails through platform data
    Passing sd power rails through platform data.
    Adding max_clk limits for all sdmmc instances.
    
    Bug 786239
    
    Original-Change-Id: I5b0e66a58ba30dbe3ed9b745df6945a6184f08aa
    Reviewed-on: http://git-master/r/18698
    Tested-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: Venkata Nageswara Penumarty <vpenumarty@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Original-Change-Id: I07b9a747ab5050e4ceff4b90eabb3d4840b46d72

--- arch/arm/mach-tegra/board-cardhu-sdhci.c
+++ arch/arm/mach-tegra/board-cardhu-sdhci.c
@@ -77,6 +77,10 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data0 = {
 	.wp_gpio = -1,
 	.power_gpio = -1,
 	.tap_delay = 6,
+	.is_voltage_switch_supported = false,
+	.vsd_name = NULL,
+	.vsd_slot_name = NULL,
+	.max_clk = 48000000,
 };
 
 static struct tegra_sdhci_platform_data tegra_sdhci_platform_data2 = {
@@ -86,6 +90,10 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data2 = {
 	.wp_gpio = -1,
 	.power_gpio = -1,
 	.tap_delay = 6,
+	.is_voltage_switch_supported = true,
+	.vsd_name = "vddio_sdmmc1",
+	.vsd_slot_name = "vddio_sd_slot",
+	.max_clk = 104000000,
 };
 
 static struct tegra_sdhci_platform_data tegra_sdhci_platform_data3 = {
@@ -95,6 +103,10 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data3 = {
 	.wp_gpio = -1,
 	.power_gpio = -1,
 	.tap_delay = 6,
+	.is_voltage_switch_supported = false,
+	.vsd_name = NULL,
+	.vsd_slot_name = NULL,
+	.max_clk = 48000000,
 };
 
 static struct platform_device tegra_sdhci_device0 = {
