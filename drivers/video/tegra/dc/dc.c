b617065a3b6ebe72f9fd2a8a79bb77cc87c3d277
Erik Gilling <konkers@android.com>
2011-02-18 16:24:37 -0800

    video: tegra: de-assert reset after clocks are turned on
    In underflow recovery, if the clocks are not enabled when the dc is brought
    out of reset writes to the DC will sometimes hang.
    
    Change-Id: If37de79f755196550018f94080ab2beed84ca326
    Signed-off-by: Erik Gilling <konkers@android.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -1024,6 +1024,9 @@ static bool _tegra_dc_enable(struct tegra_dc *dc)
 
 	clk_enable(dc->clk);
 	clk_enable(dc->emc_clk);
+	tegra_periph_reset_deassert(dc->clk);
+	msleep(10);
+
 	enable_irq(dc->irq);
 
 	tegra_dc_init(dc);
@@ -1094,10 +1097,10 @@ static void tegra_dc_reset_worker(struct work_struct *work)
 
 	msleep(100);
 	tegra_periph_reset_assert(dc->clk);
-	msleep(100);
-	tegra_periph_reset_deassert(dc->clk);
 
+	/* _tegra_dc_enable deasserts reset */
 	_tegra_dc_enable(dc);
+
 	mutex_unlock(&dc->lock);
 }
 
