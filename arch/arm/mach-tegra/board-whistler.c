19fe66d6e5db52598739caad9a8c5ff255846d05
Prashant Gaikwad <pgaikwad@nvidia.com>
2011-03-10 16:27:49 +0530

    [ARM] tegra: set pm_power_off to whistler routine
    for whistler, implement pm_power_off with max8907c's power off routine.
    
    Bug 799957
    Bug 800602 (Cold boot)
    
    Original-Change-Id: I003547d55a4203f2d04ba0086199b61d05936563
    Reviewed-on: http://git-master/r/22388
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/board-whistler.c
+++ arch/arm/mach-tegra/board-whistler.c
@@ -34,6 +34,7 @@
 #include <linux/gpio_scrollwheel.h>
 #include <linux/input.h>
 #include <linux/platform_data/tegra_usb.h>
+#include <linux/mfd/max8907c.h>
 #include <linux/memblock.h>
 
 #include <mach/clk.h>
@@ -333,6 +334,22 @@ static int __init whistler_gps_init(void)
 	return 0;
 }
 
+static void whistler_power_off(void)
+{
+	int ret;
+
+	ret = max8907c_power_off();
+	if (ret)
+		pr_err("whistler: failed to power off\n");
+
+	while (1);
+}
+
+static void __init whistler_power_off_init(void)
+{
+	pm_power_off = whistler_power_off;
+}
+
 static void __init tegra_whistler_init(void)
 {
 	char serial[20];
@@ -352,6 +369,7 @@ static void __init tegra_whistler_init(void)
 	whistler_gps_init();
 	whistler_usb_init();
 	whistler_scroll_init();
+	whistler_power_off_init();
 }
 
 int __init tegra_whistler_protected_aperture_init(void)
