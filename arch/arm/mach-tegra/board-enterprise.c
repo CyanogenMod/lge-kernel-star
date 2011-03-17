d0e6960fdbee2c317ce42e7e19cb1c479089ef48
Hiro Sugawara <hsugawara@nvidia.com>
2011-03-17 13:58:13 -0700

    arm: tegra: nvmap: Forcing to convert CarveOut requests to IOVM
    Adding a build time CONFIG option to enable forcing of conversion
    of non-IRAM CarveOut memory allocation requests to IOVM requests.
    Default is "y" to force the conversion.
    Each forced conversion is reported to console.
    Allocation alignments larger than page size for IOVM are enabled.
    Single page CarveOut allocations are converted to system memory.
    CarveOut memory reservation has been removed for aruba, cardhu,
    and enterprise.
    
    Change-Id: I3a598431d15b92ce853b3bec97be4b583d021264
    Reviewed-on: http://git-master/r/29849
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/board-enterprise.c
+++ arch/arm/mach-tegra/board-enterprise.c
@@ -381,7 +381,11 @@ static void __init tegra_enterprise_init(void)
 
 static void __init tegra_enterprise_reserve(void)
 {
+#if defined(CONFIG_NVMAP_CONVERT_CARVEOUT_TO_IOVMM)
+	tegra_reserve(0, SZ_4M, SZ_8M);
+#else
 	tegra_reserve(SZ_128M, SZ_4M, SZ_8M);
+#endif
 }
 
 MACHINE_START(TEGRA_ENTERPRISE, "tegra_enterprise")
