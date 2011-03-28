827f51770515b89e9a15d53cf282140fcb95f111
Pradeep Goudagunta <pgoudagunta@nvidia.com>
2011-03-28 16:32:40 +0530

    serial: 8250: support auto control of RTSCTS on tegra type
    Add UART_CAP_HW_CTSRTS flag to tegra type for supporting auto control
    of RTSCTS.
    
    Bug 803910
    
    Original-Change-Id: I7b69e4b203e66903ba5755338fcb55f4a87b9a43
    Reviewed-on: http://git-master/r/24463
    Tested-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/tty/serial/8250.c
+++ drivers/tty/serial/8250.c
@@ -309,7 +309,7 @@ static const struct serial8250_config uart_config[] = {
 		.tx_loadsz	= 8,
 		.fcr		= UART_FCR_ENABLE_FIFO | UART_FCR_T_TRIG_01 |
 				  UART_FCR_R_TRIG_01,
-		.flags		= UART_CAP_FIFO,
+		.flags		= UART_CAP_FIFO | UART_CAP_HW_CTSRTS,
 	},
 };
 
@@ -1874,8 +1874,13 @@ static void serial8250_set_mctrl(struct uart_port *port, unsigned int mctrl)
 		container_of(port, struct uart_8250_port, port);
 	unsigned char mcr = 0;
 
-	if (mctrl & TIOCM_RTS)
-		mcr |= UART_MCR_RTS;
+	if (up->port.type == PORT_TEGRA) {
+		if (mctrl & TIOCM_RTS)
+			mcr |= UART_MCR_HW_RTS;
+	} else {
+		if (mctrl & TIOCM_RTS)
+			mcr |= UART_MCR_RTS;
+	}
 	if (mctrl & TIOCM_DTR)
 		mcr |= UART_MCR_DTR;
 	if (mctrl & TIOCM_OUT1)
@@ -2432,6 +2437,19 @@ serial8250_do_set_termios(struct uart_port *port, struct ktermios *termios,
 
 		serial_outp(up, UART_LCR, UART_LCR_CONF_MODE_B);
 		serial_outp(up, UART_EFR, efr);
+	}
+
+	if (up->capabilities & UART_CAP_HW_CTSRTS) {
+		unsigned char mcr = serial_inp(up, UART_MCR);
+		/*
+		 * TEGRA UART core support the auto control of the RTS and CTS
+		 * flow control.
+		 */
+		if (termios->c_cflag & CRTSCTS)
+			mcr |= UART_MCR_HW_CTS;
+		else
+			mcr &= ~UART_MCR_HW_CTS;
+		serial_outp(up, UART_MCR, mcr);
 	}
 
 #ifdef CONFIG_ARCH_OMAP
