acfa81d59839add6b68f4ab384674818df413f18
Scott Williams <scwilliams@nvidia.com>
2011-03-28 00:34:42 -0700

    Update copyrights
    Original-Change-Id: I2ffeaf6f8dfeb279b40ca6f69f6c9157401a746a

--- include/linux/i2c-tegra.h
+++ include/linux/i2c-tegra.h
@@ -4,6 +4,8 @@
  * Copyright (C) 2010 Google, Inc.
  * Author: Colin Cross <ccross@android.com>
  *
+ * Copyright (C) 2010-2011 NVIDIA Corporation
+ *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
