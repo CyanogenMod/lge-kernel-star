f49fcf2f76b322c67fd0c89f738f8c0a4e3485d5
Colin Cross <ccross@android.com>
2011-04-08 18:23:33 -0700

    ARM: tegra: cpuidle: Remove fiq enable/disable
    Change-Id: I52881da51ba1daf95a960c8fc834dd2f1be824c4
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-tegra/cpuidle.c
+++ arch/arm/mach-tegra/cpuidle.c
@@ -104,7 +104,6 @@ static int tegra_idle_enter_lp2(struct cpuidle_device *dev,
 
 	local_irq_disable();
 	clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_ENTER, &dev->cpu);
-	local_fiq_disable();
 	enter = ktime_get();
 
 	idle_stats.cpu_ready_count[dev->cpu]++;
@@ -116,7 +115,6 @@ static int tegra_idle_enter_lp2(struct cpuidle_device *dev,
 	exit = ktime_sub(ktime_get(), enter);
 	us = ktime_to_us(exit);
 
-	local_fiq_enable();
 	clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_EXIT, &dev->cpu);
 	local_irq_enable();
 
