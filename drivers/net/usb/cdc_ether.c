f75b8c970c94fe95a9d21159579c782da4986a4c
Steve Lin <stlin@nvidia.com>
2011-04-20 15:15:08 -0700

    ARM: usb: Add PH450 modem to the cdc ether device list
    Adding PH450 modem to the cdc ether device list. The modem network interface
    is assigned as wwan0, which is fully supported by the GB telephony framework.
    Bug 800301
    
    Original-Change-Id: I616aa5e39a336a47c5f72baec02be6ff8d958859
    Reviewed-on: http://git-master/r/28389
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>
    Change-Id: Ice65d33a5d31b0abea4e0b8c73a02a6cca7f662c

--- drivers/net/usb/cdc_ether.c
+++ drivers/net/usb/cdc_ether.c
@@ -570,6 +570,14 @@ static const struct usb_device_id	products [] = {
 	.driver_info = (unsigned long)&wwan_info,
 },
 
+/* PH450 */
+{
+         .match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+                | USB_DEVICE_ID_MATCH_DEVICE,
+         USB_DEVICE(0x1983,0x0310),
+         .driver_info = (unsigned long)&wwan_info,
+},
+
 /*
  * WHITELIST!!!
  *
