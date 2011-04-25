79ebf828e7c711335b6fb6cf22808d0017891dc4
Dan Willemsen <dwillemsen@nvidia.com>
2011-04-25 15:18:14 -0700

    HACK: fix ventana build
    Original-Change-Id: I1cc3005f179fbb4de44c991c026b73ef1cbd9897

--- arch/arm/mach-tegra/board-ventana.c
+++ arch/arm/mach-tegra/board-ventana.c
@@ -334,19 +334,16 @@ static struct tegra_ehci_platform_data tegra_ehci_pdata[] = {
 			.phy_config = &utmi_phy_config[0],
 			.operating_mode = TEGRA_USB_HOST,
 			.power_down_on_bus_suspend = 1,
-			.usb_phy_type = TEGRA_USB_PHY_TYPE_UTMIP,
 	},
 	[1] = {
 			.phy_config = &ulpi_phy_config,
 			.operating_mode = TEGRA_USB_HOST,
 			.power_down_on_bus_suspend = 1,
-			.phy_type = TEGRA_USB_PHY_TYPE_LINK_ULPI,
 	},
 	[2] = {
 			.phy_config = &utmi_phy_config[1],
 			.operating_mode = TEGRA_USB_HOST,
 			.power_down_on_bus_suspend = 1,
-			.usb_phy_type = TEGRA_USB_PHY_TYPE_UTMIP,
 	},
 };
 
