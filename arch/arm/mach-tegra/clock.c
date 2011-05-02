8745ff4c7911d4cf21e29b8ab89202be1cda3e8d
Peter De Schrijver <pdeschrijver@nvidia.com>
2011-05-02 15:54:27 +0300

    ARM: tegra: implement events for clock tracing
    Original-Change-Id: If6ae23251aa615a678c8edb76d3c1e6463d86f2e
    
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Original-Change-Id: I50ffa54eacaf5b3973fcd6cb94eee56e46ec81bf
    Reviewed-on: http://git-master/r/30384
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>

--- arch/arm/mach-tegra/clock.c
+++ arch/arm/mach-tegra/clock.c
@@ -29,6 +29,7 @@
 #include <linux/seq_file.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <trace/events/power.h>
 
 #include <mach/clk.h>
 
@@ -255,6 +256,7 @@ int clk_enable(struct clk *c)
 
 		if (c->ops && c->ops->enable) {
 			ret = c->ops->enable(c);
+			trace_clock_enable(c->name, 1, smp_processor_id());
 			if (ret) {
 				if (c->parent)
 					clk_disable(c->parent);
@@ -284,9 +286,10 @@ void clk_disable(struct clk *c)
 		return;
 	}
 	if (c->refcnt == 1) {
-		if (c->ops && c->ops->disable)
+		if (c->ops && c->ops->disable) {
+			trace_clock_disable(c->name, 0, smp_processor_id());
 			c->ops->disable(c);
-
+		}
 		if (c->parent)
 			clk_disable(c->parent);
 
@@ -385,6 +388,7 @@ int clk_set_rate_locked(struct clk *c, unsigned long rate)
 			return ret;
 	}
 
+	trace_clock_set_rate(c->name, rate, smp_processor_id());
 	ret = c->ops->set_rate(c, rate);
 	if (ret)
 		return ret;
