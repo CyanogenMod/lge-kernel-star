971ba782ac21d2be91c66d7b3d83cc954effae4c
Bo Yan <byan@nvidia.com>
2011-05-02 15:00:26 -0700

    ARM: tegra: restore voltage to nominal when reboot
    At the time of reboot, all rails need to be set to nominal to ensure
    the success of subsequent boot.
    
    bug 821969   bug 797082
    
    Original-Change-Id: Iee635c222619dfcb3e98f13e665ea2bd04e94245
    Reviewed-on: http://git-master/r/30086
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Tested-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/dvfs.c
+++ arch/arm/mach-tegra/dvfs.c
@@ -31,6 +31,7 @@
 #include <linux/slab.h>
 #include <linux/suspend.h>
 #include <linux/delay.h>
+#include <linux/reboot.h>
 
 #include <mach/clk.h>
 
@@ -407,6 +408,23 @@ static struct notifier_block tegra_dvfs_nb = {
 	.notifier_call = tegra_dvfs_pm_notify,
 };
 
+static int tegra_dvfs_reboot_notify(struct notifier_block *nb,
+				unsigned long event, void *data)
+{
+	switch (event) {
+	case SYS_RESTART:
+	case SYS_HALT:
+	case SYS_POWER_OFF:
+		tegra_dvfs_suspend();
+		return NOTIFY_OK;
+	}
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block tegra_dvfs_reboot_nb = {
+	.notifier_call = tegra_dvfs_reboot_notify,
+};
+
 /* must be called with dvfs lock held */
 static void __tegra_dvfs_rail_disable(struct dvfs_rail *rail)
 {
@@ -493,6 +511,7 @@ int __init tegra_dvfs_late_init(void)
 	mutex_unlock(&dvfs_lock);
 
 	register_pm_notifier(&tegra_dvfs_nb);
+	register_reboot_notifier(&tegra_dvfs_reboot_nb);
 
 	return 0;
 }
