9e33c69e525bce7133903f74d69c28e95a9096ff
Haley Teng <hteng@nvidia.com>
2011-05-02 13:59:08 +0800

    ARM: tegra: Modify DDC (i2c2) clock rate as 100KHz
    Per the 8.4.1 section of HDMI spec version 1.4a, 100KHz is the maximum
    clock rate of DDC i2c bus.
    
    Bug 820552
    
    Change-Id: I7990309c4f3485c9c356623468cfabe25d733604
    Signed-off-by: Haley Teng <hteng@nvidia.com>
    Reviewed-on: http://git-master/r/29966
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Alok Chauhan <alokc@nvidia.com>

--- arch/arm/mach-tegra/board-whistler.c
+++ arch/arm/mach-tegra/board-whistler.c
@@ -161,7 +161,7 @@ static const struct tegra_pingroup_config i2c2_gen2 = {
 static struct tegra_i2c_platform_data whistler_i2c2_platform_data = {
 	.adapter_nr	= 1,
 	.bus_count	= 2,
-	.bus_clk_rate	= { 400000, 100000 },
+	.bus_clk_rate	= { 100000, 100000 },
 	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
 	.bus_mux_len	= { 1, 1 },
 };
