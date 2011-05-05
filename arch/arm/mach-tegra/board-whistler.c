0be892942bc6b27739692c2f86617d39d1af8678
Jay Cheng <jacheng@nvidia.com>
2011-05-05 16:03:52 -0400

    ARM: tegra: usb: update default UTMIP phy setting
    Revise some default settings for utimp phy
    
    Bug 815848
    
    Reviewed-on: http://git-master/r/30257
    (cherry picked from commit 5f2b525a73dd48435975ef72937d1b8627e1c917)
    
    Original-Change-Id: I403746d85a3ef32d05bec5867b407f41e1f614e4
    Reviewed-on: http://git-master/r/30568
    Tested-by: Cho-Che Cheng <jacheng@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board-whistler.c
+++ arch/arm/mach-tegra/board-whistler.c
@@ -108,7 +108,7 @@ static inline void whistler_bt_rfkill(void) { }
 
 static struct tegra_utmip_config utmi_phy_config[] = {
 	[0] = {
-			.hssync_start_delay = 0,
+			.hssync_start_delay = 9,
 			.idle_wait_delay = 17,
 			.elastic_limit = 16,
 			.term_range_adj = 6,
@@ -117,7 +117,7 @@ static struct tegra_utmip_config utmi_phy_config[] = {
 			.xcvr_lsrslew = 2,
 		},
 	[1] = {
-			.hssync_start_delay = 0,
+			.hssync_start_delay = 9,
 			.idle_wait_delay = 17,
 			.elastic_limit = 16,
 			.term_range_adj = 6,
