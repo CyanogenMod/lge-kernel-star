4e66e73dc7738ac28f99ab2bca88fe9b0c22c760
Scott Williams <scwilliams@nvidia.com>
2011-05-20 16:08:58 -0700

    ARM: tegra: power: Fix warnings when CONFIG_PM is disabled
    Original-Change-Id: I9d4b8c218cdfe6a91424b808f70c1ec056015783
    Reviewed-on: http://git-master/r/32463
    Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
    Tested-by: Niket Sirsi <nsirsi@nvidia.com>

--- drivers/usb/host/ehci-tegra.c
+++ drivers/usb/host/ehci-tegra.c
@@ -299,6 +299,7 @@ done:
 	return retval;
 }
 
+#ifdef CONFIG_PM
 static void tegra_ehci_restart(struct usb_hcd *hcd)
 {
 	struct ehci_hcd *ehci = hcd_to_ehci(hcd);
@@ -465,6 +466,7 @@ restart:
 
 	return 0;
 }
+#endif
 
 static void tegra_ehci_shutdown(struct usb_hcd *hcd)
 {
