1fcbcfbd6edca168c11965693f654b35546da014
Steve Lin <stlin@nvidia.com>
2011-05-19 17:02:51 -0700

    ARM: usb: Add enterprise A01 modem to the cdc ether device list
    Adding enterprise A01 modem to the cdc ether device list.
    Bug 800301
    
    Change-Id: Iafe75c813f0c58c9703a627fbc8b17413ac1a7ed
    Reviewed-on: http://git-master/r/32259
    Tested-by: Szming Lin <stlin@nvidia.com>
    Reviewed-by: Udaykumar Rameshchan Raval <uraval@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>

--- drivers/net/usb/cdc_ether.c
+++ drivers/net/usb/cdc_ether.c
@@ -576,6 +576,11 @@ static const struct usb_device_id	products [] = {
                 | USB_DEVICE_ID_MATCH_DEVICE,
          USB_DEVICE(0x1983,0x0310),
          .driver_info = (unsigned long)&wwan_info,
+}, {
+         .match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+                | USB_DEVICE_ID_MATCH_DEVICE,
+         USB_DEVICE(0x1983,0x0321),
+         .driver_info = (unsigned long)&wwan_info,
 },
 
 /*
