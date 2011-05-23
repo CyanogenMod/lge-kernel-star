02bd625003aadd7837cf6b7ee6f1a2e67c11de9b
Laxman Dewangan <ldewangan@nvidia.com>
2011-05-23 14:33:14 +0530

    arm: tegra: gpio: Implementation for gpio_to_irq
    Adding the support in function gpio_to_irq() such that the external
    gpio peripheral driver can provide the gpio to irq number through
    their gpio implementation.
    
    Original-Change-Id: Ibab369df435af79ce09d71e491a8ee851f2228ba
    Reviewed-on: http://git-master/r/32563
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Tested-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/include/mach/gpio.h
+++ arch/arm/mach-tegra/include/mach/gpio.h
@@ -37,16 +37,23 @@
 
 static inline int gpio_to_irq(unsigned int gpio)
 {
+	/* SOC gpio */
 	if (gpio < TEGRA_NR_GPIOS)
 		return INT_GPIO_BASE + gpio;
-	return -EINVAL;
+
+	/* For non soc gpio, the external peripheral driver need to
+	 * provide the implementation */
+	return __gpio_to_irq(gpio);
 }
 
 static inline int irq_to_gpio(unsigned int irq)
 {
+	/* SOC gpio */
 	if ((irq >= INT_GPIO_BASE) && (irq < INT_GPIO_BASE + INT_GPIO_NR))
 		return irq - INT_GPIO_BASE;
-	return -EINVAL;
+
+	/* we don't supply reverse mappings for non-SOC gpios */
+	return -EIO;
 }
 
 struct tegra_gpio_table {
