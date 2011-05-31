206be5924f3ac49ad4ce02eaf119bdfe819d9734
Rakesh Bodla <rbodla@nvidia.com>
2011-05-31 15:18:00 +0530

    ARM: tegra: Enable AHB prefetch for USB
    Enabling AHB prefetch on USB1, USB2, USB3 controllers,
    to improve the USB transfer throughput.
    
    Bug 820602
    
    Reviewed-on: http://git-master/r/30475
    (cherry picked from commit f982fa0f0878a8f4bc1739bf047313171d3cf38d)
    
    Original-Change-Id: Iec4e877136804d023177a8689df20cc1f19cda7c
    Reviewed-on: http://git-master/r/34464
    Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
    Tested-by: Niket Sirsi <nsirsi@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -28,6 +28,7 @@
 #include <linux/notifier.h>
 #include <linux/reboot.h>
 #include <linux/mqueue.h>
+#include <linux/bitops.h>
 
 #include <asm/hardware/cache-l2x0.h>
 #include <asm/system.h>
@@ -45,6 +46,33 @@
 #include "pm.h"
 
 #define MC_SECURITY_CFG2	0x7c
+
+#define AHB_ARBITRATION_PRIORITY_CTRL		0x4
+#define   AHB_PRIORITY_WEIGHT(x)	(((x) & 0x7) << 29)
+#define   PRIORITY_SELECT_USB	BIT(6)
+#define   PRIORITY_SELECT_USB2	BIT(18)
+#define   PRIORITY_SELECT_USB3	BIT(17)
+
+#define AHB_GIZMO_AHB_MEM		0xc
+#define   ENB_FAST_REARBITRATE	BIT(2)
+
+#define AHB_GIZMO_USB		0x1c
+#define AHB_GIZMO_USB2		0x78
+#define AHB_GIZMO_USB3		0x7c
+#define   IMMEDIATE	BIT(18)
+
+#define AHB_MEM_PREFETCH_CFG3	0xe0
+#define AHB_MEM_PREFETCH_CFG4	0xe4
+#define AHB_MEM_PREFETCH_CFG1	0xec
+#define AHB_MEM_PREFETCH_CFG2	0xf0
+#define   PREFETCH_ENB	BIT(31)
+#define   MST_ID(x)	(((x) & 0x1f) << 26)
+#define   AHBDMA_MST_ID	MST_ID(5)
+#define   USB_MST_ID	MST_ID(6)
+#define   USB2_MST_ID	MST_ID(18)
+#define   USB3_MST_ID	MST_ID(17)
+#define   ADDR_BNDRY(x)	(((x) & 0xf) << 21)
+#define   INACTIVITY_TIMEOUT(x)	(((x) & 0xffff) << 0)
 
 unsigned long tegra_bootloader_fb_start;
 unsigned long tegra_bootloader_fb_size;
@@ -168,6 +196,62 @@ static void __init tegra_init_power(void)
 #endif
 }
 
+static inline unsigned long gizmo_readl(unsigned long offset)
+{
+	return readl(IO_TO_VIRT(TEGRA_AHB_GIZMO_BASE + offset));
+}
+
+static inline void gizmo_writel(unsigned long value, unsigned long offset)
+{
+	writel(value, IO_TO_VIRT(TEGRA_AHB_GIZMO_BASE + offset));
+}
+
+static void __init tegra_init_ahb_gizmo_settings(void)
+{
+	unsigned long val;
+
+	val = gizmo_readl(AHB_GIZMO_AHB_MEM);
+	val |= ENB_FAST_REARBITRATE;
+	gizmo_writel(val, AHB_GIZMO_AHB_MEM);
+
+	val = gizmo_readl(AHB_GIZMO_USB);
+	val |= IMMEDIATE;
+	gizmo_writel(val, AHB_GIZMO_USB);
+
+	val = gizmo_readl(AHB_GIZMO_USB2);
+	val |= IMMEDIATE;
+	gizmo_writel(val, AHB_GIZMO_USB2);
+
+	val = gizmo_readl(AHB_GIZMO_USB3);
+	val |= IMMEDIATE;
+	gizmo_writel(val, AHB_GIZMO_USB3);
+
+	val = gizmo_readl(AHB_ARBITRATION_PRIORITY_CTRL);
+	val |= PRIORITY_SELECT_USB | PRIORITY_SELECT_USB2 | PRIORITY_SELECT_USB3
+				| AHB_PRIORITY_WEIGHT(7);
+	gizmo_writel(val, AHB_ARBITRATION_PRIORITY_CTRL);
+
+	val = gizmo_readl(AHB_MEM_PREFETCH_CFG1);
+	val &= ~MST_ID(~0);
+	val |= PREFETCH_ENB | AHBDMA_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	gizmo_writel(val, AHB_MEM_PREFETCH_CFG1);
+
+	val = gizmo_readl(AHB_MEM_PREFETCH_CFG2);
+	val &= ~MST_ID(~0);
+	val |= PREFETCH_ENB | USB_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	gizmo_writel(val, AHB_MEM_PREFETCH_CFG2);
+
+	val = gizmo_readl(AHB_MEM_PREFETCH_CFG3);
+	val &= ~MST_ID(~0);
+	val |= PREFETCH_ENB | USB3_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	gizmo_writel(val, AHB_MEM_PREFETCH_CFG3);
+
+	val = gizmo_readl(AHB_MEM_PREFETCH_CFG4);
+	val &= ~MST_ID(~0);
+	val |= PREFETCH_ENB | USB2_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	gizmo_writel(val, AHB_MEM_PREFETCH_CFG4);
+}
+
 static bool console_flushed;
 
 static int tegra_pm_flush_console(struct notifier_block *this,
@@ -219,6 +303,7 @@ void __init tegra_init_early(void)
 #ifndef CONFIG_ARCH_TEGRA_2x_SOC
 	tegra_mc_init(); /* !!!FIXME!!! Change Tegra3 behavior to match Tegra2 */
 #endif
+	tegra_init_ahb_gizmo_settings();
 }
 
 static int __init tegra_lp0_vec_arg(char *options)
