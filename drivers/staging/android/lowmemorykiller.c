2712fb156b48cb71c366115efeb58b436afe0aac
Ricardo Cerqueira <github@cerqueira.org>
2011-06-06 00:05:44 +0100

    Revert "lowmemorykiller: only check cache for thresholds"
    This reverts commit 45463d5f4f5c6169baf96089656dd434680586e0.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -110,7 +110,8 @@ static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-		if (other_file < lowmem_minfree[i]) {
+		if (other_free < lowmem_minfree[i] &&
+		    other_file < lowmem_minfree[i]) {
 			min_adj = lowmem_adj[i];
 			break;
 		}
