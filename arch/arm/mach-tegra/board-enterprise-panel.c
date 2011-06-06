71e92333d8074f4afd9fced55931ca11b84eeb44
Wen Yi <wyi@nvidia.com>
2011-06-06 13:51:12 -0700

    arm: tegra: set cpufreq governor parameter
    The new parameters help cpu frequency stay in low level
    clock as much as possible. This helps reduce power consumption
    when display is off.
    
    BUG 817727
    
    Change-Id: I030319d78e793a0372008a5c0640dce3720a47d8
    Reviewed-on: http://git-master/r/35266
    Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
    Tested-by: Niket Sirsi <nsirsi@nvidia.com>

--- arch/arm/mach-tegra/board-enterprise-panel.c
+++ arch/arm/mach-tegra/board-enterprise-panel.c
@@ -433,6 +433,9 @@ static void enterprise_panel_early_suspend(struct early_suspend *h)
 #ifdef CONFIG_TEGRA_CONVSERVATIVE_GOV_ON_EARLYSUPSEND
 	cpufreq_save_default_governor();
 	cpufreq_set_conservative_governor();
+	cpufreq_set_conservative_governor_param(
+		SET_CONSERVATIVE_GOVERNOR_UP_THRESHOLD,
+		SET_CONSERVATIVE_GOVERNOR_DOWN_THRESHOLD);
 #endif
 }
 
