GalaxyTab101/samsung-kernel-galaxytab101

arm: tegra: Clean up SOC conditionals
Change SOC conditionals to make them more forward-looking.

Change-Id: Ib60db4e690c2f396afdec962616d735548b5a8a9
Reviewed-on: http://git-master/r/32706
Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
Tested-by: Niket Sirsi <nsirsi@nvidia.com>

arch/arm/mach-tegra/include/mach/memory.h
@@ -24,10 +24,8 @@
 /* physical offset of RAM */
 #if defined(CONFIG_ARCH_TEGRA_2x_SOC)
 #define PHYS_OFFSET		UL(0)
-#elif defined(CONFIG_ARCH_TEGRA_3x_SOC)
-#define PHYS_OFFSET		UL(0x80000000)
 #else
-#error "Invalid Tegra SoC family selection"
+#define PHYS_OFFSET		UL(0x80000000)
 #endif
 
 /*

CyanogenMod/lge-kernel-star

Original-Change-Id: Ib60db4e690c2f396afdec962616d735548b5a8a9

arch/arm/mach-tegra/include/mach/memory.h
@@ -24,10 +24,8 @@
 /* physical offset of RAM */
 #if defined(CONFIG_ARCH_TEGRA_2x_SOC)
 #define PLAT_PHYS_OFFSET		UL(0)
-#elif defined(CONFIG_ARCH_TEGRA_3x_SOC)
-#define PLAT_PHYS_OFFSET		UL(0x80000000)
 #else
-#error "Invalid Tegra SoC family selection"
+#define PLAT_PHYS_OFFSET		UL(0x80000000)
 #endif
 
 /*