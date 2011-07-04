76a2c11b59aec061e3ee4ac3e0380629f0b364e1
Suresh Mangipudi <smangipudi@nvidia.com>
2011-07-04 11:14:19 +0530

    arm: tegra: otg: Remove host stop
    Remove the stop host call when device is entering suspend. Need to be
    detect the host device even after the device exits from LP state.
    
    Bug 835678
    
    Reviewed-on: http://git-master/r/39506
    (cherry picked from commit 868eff5bd918be16ce7181a9f7efeb3d061370c7)
    
    Original-Change-Id: I4b4150925f1c19463a58c73ca3dfc5c900bf83cf
    Reviewed-on: http://git-master/r/44307
    Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Tested-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Tested-by: Bala Murali Krishna <balam@nvidia.com>
    Reviewed-by: Hanumanth Venkateswa Moganty <vmoganty@nvidia.com>

--- drivers/usb/otg/tegra-otg.c
+++ drivers/usb/otg/tegra-otg.c
@@ -387,12 +387,9 @@ static int tegra_otg_suspend(struct platform_device *pdev, pm_message_t state)
 	tegra_otg->intr_reg_data = readl(tegra_otg->regs + USB_PHY_WAKEUP);
 	clk_disable(tegra_otg->clk);
 
-	if (from == OTG_STATE_A_HOST)
-		tegra_stop_host(tegra_otg);
-	else if (from == OTG_STATE_B_PERIPHERAL && otg->gadget)
+	if (from == OTG_STATE_B_PERIPHERAL && otg->gadget)
 		usb_gadget_vbus_disconnect(otg->gadget);
 
-	otg->state = OTG_STATE_A_SUSPEND;
 	tegra_otg_disable_clk();
 	return 0;
 }
