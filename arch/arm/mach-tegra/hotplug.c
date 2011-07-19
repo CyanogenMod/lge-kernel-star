5f9c7f203c3bd12db0c899db1822811238c702b5
Scott Williams <scwilliams@nvidia.com>
2011-07-19 15:13:40 -0700

    ARM: tegra2: Don't use tegra_cpu_save to exit coherency on reset
    Change tegra2_sleep_reset not to depend on tegra_cpu_save to exit
    coherency.
    
    Now tegra2_sleep_reset no longer depends on CONFIG_PM_SLEEP
    
    Change-Id: I478e723e826fd3ddbd6a6e8bceaedf795bd2ee26
    Signed-off-by: Scott Williams <scwilliams@nvidia.com>
    DW: Split into logical changes
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/hotplug.c
+++ arch/arm/mach-tegra/hotplug.c
@@ -1,7 +1,7 @@
 /*
- *  linux/arch/arm/mach-tegra/hotplug.c
+ *  arch/arm/mach-tegra/hotplug.c
  *
- *  Copyright (C) 2010 NVIDIA Corporation
+ *  Copyright (C) 2010-2011 NVIDIA Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -12,6 +12,7 @@
 #include <linux/smp.h>
 
 #include <asm/cpu_pm.h>
+#include <asm/cacheflush.h>
 
 #include <mach/iomap.h>
 
@@ -51,7 +52,11 @@ int platform_cpu_kill(unsigned int cpu)
 
 void platform_cpu_die(unsigned int cpu)
 {
+	/* Flush the L1 data cache. */
+	flush_cache_all();
+
 #ifdef CONFIG_ARCH_TEGRA_2x_SOC
+	/* Place the current CPU in reset. */
 	tegra2_sleep_reset();
 #endif
 
