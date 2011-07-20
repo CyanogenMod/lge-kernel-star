518582e6e8fae0b4c8b466cd5dfe82b851903113
Alex Frid <afrid@nvidia.com>
2011-07-20 15:04:06 -0700

    regulator: Expand regulator notifier events
    Add regulator enable notifications (not sent by the current code).
    Add voltage notifications sent only if regulator output is actually
    changed (keep intact existing voltage change notification that is
    sent unconditionally - even if voltage change was not allowed by
    constraints, or no actual change since regulator is disabled):
    
    PRE_ENABLE     Regulator is to be enabled
    POST_ENABLE    Regulator was enabled
    OUT_PRECHANGE  Regulator is enabled and its voltage is to be changed
    OUT_POSTCHANGE Regulator is enabled and its voltage was changed
    
    Bug 853132
    
    Original-Change-Id: I2759ada1c5d1e43f746f6c866f9b3c203b68aed6
    Reviewed-on: http://git-master/r/42262
    Tested-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: Karan Jhavar <kjhavar@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1327,6 +1327,8 @@ static int _regulator_enable(struct regulator_dev *rdev)
 			}
 
 			trace_regulator_enable(rdev_get_name(rdev));
+			_notifier_call_chain(
+				rdev, REGULATOR_EVENT_PRE_ENABLE, NULL);
 
 			/* Allow the regulator to ramp; it would be useful
 			 * to extend this for bulk operations so that the
@@ -1344,6 +1346,8 @@ static int _regulator_enable(struct regulator_dev *rdev)
 				udelay(delay);
 			}
 
+			_notifier_call_chain(
+				rdev, REGULATOR_EVENT_POST_ENABLE, NULL);
 			trace_regulator_enable_complete(rdev_get_name(rdev));
 
 		} else if (ret < 0) {
@@ -1637,6 +1641,10 @@ static int _regulator_do_set_voltage(struct regulator_dev *rdev,
 
 	trace_regulator_set_voltage(rdev_get_name(rdev), min_uV, max_uV);
 
+	if (_regulator_is_enabled(rdev))
+		_notifier_call_chain(rdev, REGULATOR_EVENT_OUT_PRECHANGE,
+				     NULL);
+
 	if (rdev->desc->ops->set_voltage) {
 		ret = rdev->desc->ops->set_voltage(rdev, min_uV, max_uV,
 						   &selector);
@@ -1702,6 +1710,10 @@ static int _regulator_do_set_voltage(struct regulator_dev *rdev,
 
 	if (ret == 0)
 		_notifier_call_chain(rdev, REGULATOR_EVENT_VOLTAGE_CHANGE,
+				     NULL);
+
+	if (_regulator_is_enabled(rdev))
+		_notifier_call_chain(rdev, REGULATOR_EVENT_OUT_POSTCHANGE,
 				     NULL);
 
 	trace_regulator_set_voltage_complete(rdev_get_name(rdev), selector);
