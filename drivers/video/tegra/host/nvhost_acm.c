6d02a32d4b7cfd4df1d64fbd2817494af5912448
Terje Bergstrom <tbergstrom@nvidia.com>
2011-07-27 08:26:50 +0300

    nvhost: Disable 3D power gating due to hangs
    After 3D power gating was enabled, the system has exhibited hanging sync
    points. This patch disables 3D power gating for all systems.
    
    Bug 855392,855889
    
    Original-Change-Id: I35c933e31223aacbc2a088793e5c799f2f1ff0ec
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/43427
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Andrew Howe <ahowe@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>

--- drivers/video/tegra/host/nvhost_acm.c
+++ drivers/video/tegra/host/nvhost_acm.c
@@ -122,14 +122,10 @@ static const char *get_module_clk_id(const char *module, int index)
 	return NULL;
 }
 
-/* Not all hardware revisions support power gating */
+/* 3D power gating disabled as it causes syncpt hangs */
 static bool _3d_powergating_disabled(void)
 {
-	int chipid = tegra_get_chipid();
-
-	return chipid < TEGRA_CHIPID_TEGRA3
-		|| (chipid == TEGRA_CHIPID_TEGRA3
-			&& tegra_get_revision() == TEGRA_REVISION_A01);
+	return 1;
 }
 
 int nvhost_module_init(struct nvhost_module *mod, const char *name,
