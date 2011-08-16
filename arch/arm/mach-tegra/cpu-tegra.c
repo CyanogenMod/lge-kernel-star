7069e25559b71c6c3a215a3c6bb55f10773763c7
Peter Boonstoppel <pboonstoppel@nvidia.com>
2011-08-16 12:01:29 -0700

    ARM: tegra: power: Don't clip EDP limits to cpufreq tables
    Always use maximum possible frequency when applying EDP
    capping. Toggled through CONFIG_TEGRA_EDP_EXACT_FREQ.
    
    Bug 863761
    
    Original-Change-Id: I327440546991ad4f3abc78100a3a18017f3464b6
    Reviewed-on: http://git-master/r/47169
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -106,15 +106,18 @@ static unsigned int edp_limit;
 
 static void edp_update_limit(void)
 {
-	int i;
 	unsigned int limit = cpumask_weight(&edp_cpumask);
 
 	if (!cpu_edp_limits)
 		return;
 
 	BUG_ON((edp_thermal_index >= cpu_edp_limits_size) || (limit == 0));
+#ifdef CONFIG_TEGRA_EDP_EXACT_FREQ
+	edp_limit = cpu_edp_limits[edp_thermal_index].freq_limits[limit - 1];
+#else
 	limit = cpu_edp_limits[edp_thermal_index].freq_limits[limit - 1];
 
+	int i;
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
 		if (freq_table[i].frequency > limit) {
 			break;
@@ -122,6 +125,7 @@ static void edp_update_limit(void)
 	}
 	BUG_ON(i == 0);	/* min freq above the limit or table empty */
 	edp_limit = freq_table[i-1].frequency;
+#endif
 }
 
 static unsigned int edp_governor_speed(unsigned int requested_speed)
