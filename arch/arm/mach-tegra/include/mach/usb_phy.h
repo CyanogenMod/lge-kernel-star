c0dec6c0ba0c2a9ede3ae8f81392ed9ecfd7eb93
Seshendra Gadagottu <sgadagottu@nvidia.com>
2011-08-16 15:09:31 +0530

    tegra: usb: phy: preresume action for HSIC
    Added preresume action for HSIC. This is needed for
    modem L2 state.
    
    BUG 828389
    
    Original-Change-Id: Ifbb9105741f87840d6b607109bc448704600903f
    Reviewed-on: http://git-master/r/47296
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/include/mach/usb_phy.h
+++ arch/arm/mach-tegra/include/mach/usb_phy.h
@@ -60,6 +60,7 @@ struct tegra_uhsic_config {
 	u8 elastic_underrun_limit;
 	u8 elastic_overrun_limit;
 	int (*postsuspend)(void);
+	int (*preresume)(void);
 };
 
 enum tegra_usb_phy_port_speed {
--- arch/arm/mach-tegra/usb_phy.c
+++ arch/arm/mach-tegra/usb_phy.c
@@ -1274,6 +1274,16 @@ static int uhsic_phy_postsuspend(struct tegra_usb_phy *phy, bool is_dpd)
 	return 0;
 }
 
+static int uhsic_phy_preresume(struct tegra_usb_phy *phy, bool is_dpd)
+{
+	struct tegra_uhsic_config *uhsic_config = phy->config;
+
+	if (uhsic_config->preresume)
+		uhsic_config->preresume();
+
+	return 0;
+}
+
 static int uhsic_phy_postresume(struct tegra_usb_phy *phy, bool is_dpd)
 {
 #ifdef CONFIG_ARCH_TEGRA_2x_SOC
@@ -2097,7 +2107,7 @@ void tegra_usb_phy_preresume(struct tegra_usb_phy *phy, bool is_dpd)
 		utmi_phy_preresume,
 		NULL,
 		NULL,
-		NULL,
+		uhsic_phy_preresume,
 	};
 
 	if (preresume[phy->usb_phy_type])
