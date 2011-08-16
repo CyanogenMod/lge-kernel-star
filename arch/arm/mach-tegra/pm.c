4bf278281247c5c56c61c2de24864073a4cdeb30
Jin Qian <jqian@nvidia.com>
2011-08-15 19:32:23 -0700

    ARM: tegra: power: setup TTB0 for cacheable memory
    Bug 862494
    
    Change-Id: Ib7875ded150b3e9dc288a9ed90f6ded0a37014a3
    Reviewed-on: http://git-master/r/47246
    Tested-by: Jin Qian <jqian@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Daniel Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/pm.c
+++ arch/arm/mach-tegra/pm.c
@@ -240,7 +240,8 @@ static __init int create_suspend_pgtable(void)
 	identity_mapping_add(tegra_pgd, IO_IRAM_PHYS,
 		IO_IRAM_PHYS + SECTION_SIZE);
 
-	tegra_pgd_phys = virt_to_phys(tegra_pgd);
+	/* inner/outer write-back/write-allocate, sharable */
+	tegra_pgd_phys = (virt_to_phys(tegra_pgd) & PAGE_MASK) | 0x4A;
 
 	return 0;
 }
