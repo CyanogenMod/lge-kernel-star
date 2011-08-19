7253a4c7723b8e1e14a2e97cfcd9f4e0c483fdb1
Mayuresh Kulkarni <mkulkarni@nvidia.com>
2011-08-19 19:22:17 +0530

    arm: tegra: pinmux: fix a few warnings for Tegra2
    APIs lock_name(), od_name(), ioreset_name() are called from code for
    Tegra3 and above. However, their implementation was not taking care
    of this. This was causing 3 warnings during Tegra2 builds.
    
    Change-Id: I4ac4d394c68fd1f8bab5938b2af76c8b92d04a64
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Reviewed-on: http://git-master/r/48195
    Reviewed-by: Daniel Willemsen <dwillemsen@nvidia.com>
    Tested-by: Daniel Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/pinmux.c
+++ arch/arm/mach-tegra/pinmux.c
@@ -217,6 +217,7 @@ static const char *pupd_name(unsigned long val)
 	}
 }
 
+#if !defined(CONFIG_ARCH_TEGRA_2x_SOC)
 static const char *lock_name(unsigned long val)
 {
 	switch(val) {
@@ -264,6 +265,7 @@ static const char *ioreset_name(unsigned long val)
 		return "IO_RESET_DEFAULT";
 	}
 }
+#endif
 
 #if defined(TEGRA_PINMUX_HAS_IO_DIRECTION)
 static const char *io_name(unsigned long val)
