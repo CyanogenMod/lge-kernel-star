6d849060af63c9c497156650d6765154e484ca5e
Kevin Huang <kevinh@nvidia.com>
2011-08-19 03:50:04 -0700

    ARM: tegra: enterprise: Fix mode check in board config file.
    Original-Change-Id: Ia19646893c7a25a42538a5aafab2a13bd6bbc5dc
    Reviewed-on: http://git-master/r/48128
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/board-enterprise-panel.c
+++ arch/arm/mach-tegra/board-enterprise-panel.c
@@ -503,7 +503,7 @@ static int enterprise_dsi_panel_postsuspend(void)
 static struct tegra_dsi_cmd dsi_init_cmd[]= {
 	DSI_CMD_SHORT(0x05, 0x11, 0x00),
 	DSI_DLY_MS(150),
-#if(DC_CTRL_MODE == TEGRA_DC_OUT_ONE_SHOT_MODE)
+#if(DC_CTRL_MODE & TEGRA_DC_OUT_ONE_SHOT_MODE)
 	DSI_CMD_SHORT(0x15, 0x35, 0x00),
 #endif
 	DSI_CMD_SHORT(0x05, 0x29, 0x00),
@@ -513,13 +513,13 @@ static struct tegra_dsi_cmd dsi_init_cmd[]= {
 static struct tegra_dsi_cmd dsi_early_suspend_cmd[] = {
 	DSI_CMD_SHORT(0x05, 0x28, 0x00),
 	DSI_DLY_MS(20),
-#if(DC_CTRL_MODE == TEGRA_DC_OUT_ONE_SHOT_MODE)
+#if(DC_CTRL_MODE & TEGRA_DC_OUT_ONE_SHOT_MODE)
 	DSI_CMD_SHORT(0x05, 0x34, 0x00),
 #endif
 };
 
 static struct tegra_dsi_cmd dsi_late_resume_cmd[] = {
-#if(DC_CTRL_MODE == TEGRA_DC_OUT_ONE_SHOT_MODE)
+#if(DC_CTRL_MODE & TEGRA_DC_OUT_ONE_SHOT_MODE)
 	DSI_CMD_SHORT(0x15, 0x35, 0x00),
 #endif
 	DSI_CMD_SHORT(0x05, 0x29, 0x00),
@@ -529,7 +529,7 @@ static struct tegra_dsi_cmd dsi_late_resume_cmd[] = {
 static struct tegra_dsi_cmd dsi_suspend_cmd[] = {
 	DSI_CMD_SHORT(0x05, 0x28, 0x00),
 	DSI_DLY_MS(20),
-#if(DC_CTRL_MODE == TEGRA_DC_OUT_ONE_SHOT_MODE)
+#if(DC_CTRL_MODE & TEGRA_DC_OUT_ONE_SHOT_MODE)
 	DSI_CMD_SHORT(0x05, 0x34, 0x00),
 #endif
 	DSI_CMD_SHORT(0x05, 0x10, 0x00),
@@ -706,7 +706,7 @@ int __init enterprise_panel_init(void)
 	gpio_direction_output(enterprise_lcd_swp_pl, 0);
 	enterprise_stereo_set_orientation(enterprise_stereo.orientation);
 
-#if(DC_CTRL_MODE != TEGRA_DC_OUT_ONE_SHOT_MODE)
+#if !(DC_CTRL_MODE & TEGRA_DC_OUT_ONE_SHOT_MODE)
 	tegra_gpio_enable(enterprise_lcd_te);
 	gpio_request(enterprise_lcd_swp_pl, "lcd_te");
 	gpio_direction_input(enterprise_lcd_te);
