d7dcda2bdb0fac5403e60ffdb19be858678d2135
Alexandre Berdery <aberdery@nvidia.com>
2011-08-25 12:41:47 +0200

    arm:tegra:enterprise: Export MODEM_PWR_ON & MODEM_RESET GPIOs
    RIL and FIL require GPIOs access from user space.
    This is possible with this change using sysfs.
    
    Bug 866051
    
    Original-Change-Id: Ie6ee4e305ae709a4eae467e27b269b9327031a17
    Reviewed-on: http://git-master/r/49206
    Tested-by: Alexandre Berdery <aberdery@nvidia.com>
    Reviewed-by: Frederic Bossy <fbossy@nvidia.com>
    Reviewed-by: Szming Lin <stlin@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board-enterprise-baseband.c
+++ arch/arm/mach-tegra/board-enterprise-baseband.c
@@ -291,7 +291,11 @@ static int __init ph450_init(void)
 	tegra_gpio_enable(BB_RST_OUT);
 
 	gpio_direction_output(MODEM_PWR_ON, 0);
+	/* export GPIO for user space access through sysfs */
+	gpio_export(MODEM_PWR_ON, false);
 	gpio_direction_output(MODEM_RESET, 0);
+	/* export GPIO for user space access through sysfs */
+	gpio_export(MODEM_RESET, false);
 	gpio_direction_output(AP2MDM_ACK2, 1);
 	gpio_direction_input(MDM2AP_ACK2);
 	gpio_direction_input(BB_RST_OUT);
