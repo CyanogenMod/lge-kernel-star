6a06b17cc41a590cc1f5c85a1bb3a82772578a5e
Tom Cherry <tcherry@nvidia.com>
2011-09-07 17:10:38 -0700

    arm: tegra: cardhu: cleanup ram console
    Various cleanup including removing two unneeded static variables and
    an unneeded function, as well as reserving the ram console after the
    carveout and framebuffer.
    
    Reviewed-on: http://git-master/r/51214
    (cherry-picked from commit cb64054a12aca84b097556c7c4f4e7c67916fd93)
    
    Change-Id: I2b950a6a8637beed3910cc59f1e8028dc3c10562
    Reviewed-on: http://git-master/r/55086
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/board-cardhu.c
+++ arch/arm/mach-tegra/board-cardhu.c
@@ -64,9 +64,6 @@
 #include "pm.h"
 #include "baseband-xmm-power.h"
 
-static unsigned long ramconsole_start;
-static unsigned long ramconsole_size;
-
 static struct usb_mass_storage_platform_data tegra_usb_fsg_platform = {
 	.vendor = "NVIDIA",
 	.product = "Tegra 3",
@@ -1012,14 +1009,6 @@ static void cardhu_sata_init(void)
 static void cardhu_sata_init(void) { }
 #endif
 
-static void cardhu_ramconsole_init(void)
-{
-	struct resource *res;
-	res = platform_get_resource(&ram_console_device, IORESOURCE_MEM, 0);
-	res->start = ramconsole_start;
-	res->end = res->start + ramconsole_size - 1;
-}
-
 static void __init tegra_cardhu_init(void)
 {
 	tegra_clk_init_from_table(cardhu_clk_init_table);
@@ -1033,7 +1022,6 @@ static void __init tegra_cardhu_init(void)
 	cardhu_uart_init();
 	snprintf(usb_serial_num, sizeof(usb_serial_num), "%llx", tegra_chip_uid());
 	andusb_plat.serial_number = kstrdup(usb_serial_num, GFP_KERNEL);
-	cardhu_ramconsole_init();
 	cardhu_tsensor_init();
 	platform_add_devices(cardhu_devices, ARRAY_SIZE(cardhu_devices));
 	cardhu_sdhci_init();
@@ -1059,22 +1047,34 @@ static void __init tegra_cardhu_init(void)
 	tegra_release_bootloader_fb();
 }
 
-static void __init tegra_cardhu_reserve(void)
+static void __init cardhu_ramconsole_reserve(unsigned long size)
 {
+	struct resource *res;
 	long ret;
-	ramconsole_size = SZ_1M;
-	ramconsole_start = memblock_end_of_DRAM() - ramconsole_size;
-	ret = memblock_remove(ramconsole_start, ramconsole_size);
+
+	res = platform_get_resource(&ram_console_device, IORESOURCE_MEM, 0);
+	if (!res) {
+		pr_err("Failed to find memory resource for ram console\n");
+		return;
+	}
+	res->start = memblock_end_of_DRAM() - size;
+	res->end = res->start + size - 1;
+	ret = memblock_remove(res->start, size);
 	if (ret) {
-		ramconsole_size = 0;
+		ram_console_device.resource = NULL;
+		ram_console_device.num_resources = 0;
 		pr_err("Failed to reserve memory block for ram console\n");
 	}
+}
 
+static void __init tegra_cardhu_reserve(void)
+{
 #if defined(CONFIG_NVMAP_CONVERT_CARVEOUT_TO_IOVMM)
 	tegra_reserve(0, SZ_8M, SZ_8M);
 #else
 	tegra_reserve(SZ_128M, SZ_8M, SZ_8M);
 #endif
+	cardhu_ramconsole_reserve(SZ_1M);
 }
 
 MACHINE_START(CARDHU, "cardhu")
