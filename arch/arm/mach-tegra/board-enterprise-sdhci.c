041df6fbf7c5792e0938bc60fc9b747705fd8df8
Sachin Nikam <snikam@nvidia.com>
2011-09-20 14:15:48 +0530

    arm: tegra: enterprise: built_in true for eMMC and SDIO
    Making built_in=1 in the mmc platform data for Internal eMMC and SDIO card.
    
    Change-Id: I9caa697a60b823733b953f99bf774a4aec41c61a
    Signed-off-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-on: http://git-master/r/53404
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>

--- arch/arm/mach-tegra/board-enterprise-sdhci.c
+++ arch/arm/mach-tegra/board-enterprise-sdhci.c
@@ -116,6 +116,7 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data0 = {
 	.mmc_data = {
 		.register_status_notify	= enterprise_wifi_status_register,
 		.embedded_sdio = &embedded_sdio_data0,
+		.built_in = 1,
 	},
 	.cd_gpio = -1,
 	.wp_gpio = -1,
@@ -132,6 +133,9 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data3 = {
 	.cd_gpio = -1,
 	.wp_gpio = -1,
 	.power_gpio = -1,
+	.mmc_data = {
+		.built_in = 1,
+	}
 };
 
 static struct platform_device tegra_sdhci_device0 = {
