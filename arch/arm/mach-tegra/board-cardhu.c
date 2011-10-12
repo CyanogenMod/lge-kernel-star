6e916a80482e7b9f9f44d355e1e80b5ab62ddcb1
Alok Chauhan <alokc@nvidia.com>
2011-10-12 14:31:04 +0530

    arm: tegra: cardhu: change i2c pll src to pll_p
    Change the pll source of i2c from CLK_M to PLL_P_OUT0.
    
    Bug 856468
    
    Change-Id: I0865d0679fcc7ae2b862b9f68900275ab05da5ec
    Reviewed-on: http://git-master/r/57505
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/board-cardhu.c
+++ arch/arm/mach-tegra/board-cardhu.c
@@ -210,6 +210,11 @@ static __initdata struct tegra_clk_init_table cardhu_clk_init_table[] = {
 	{ "spdif_out",	"pll_a_out0",	0,		false},
 	{ "sdmmc3",	"clk_m",	12000000,	true},
 	{ "vi_sensor",	"pll_p",	150000000,	false},
+	{ "i2c1",	"pll_p",	3200000,	false},
+	{ "i2c2",	"pll_p",	3200000,	false},
+	{ "i2c3",	"pll_p",	3200000,	false},
+	{ "i2c4",	"pll_p",	3200000,	false},
+	{ "i2c5",	"pll_p",	3200000,	false},
 	{ NULL,		NULL,		0,		0},
 };
 
