30f383ff934733ea384ce24100b72a479a8c57b3
Karthik Ramakrishnan <karthikr@nvidia.com>
2011-11-30 15:41:55 -0800

    arm: tegra: cardhu: Remove conservative gov on suspend
    Cardhu does not require conservative governor on early
    suspend. It is also disabled in the defconfig
    
    Change-Id: I5daa0fdcb8ef697bed83756e97f06747e5c89f8c
    Signed-off-by: Karthik Ramakrishnan <karthikr@nvidia.com>
    Reviewed-on: http://git-master/r/67326
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>

--- arch/arm/mach-tegra/board-cardhu-panel.c
+++ arch/arm/mach-tegra/board-cardhu-panel.c
@@ -1055,21 +1055,11 @@ static void cardhu_panel_early_suspend(struct early_suspend *h)
 		fb_blank(registered_fb[0], FB_BLANK_POWERDOWN);
 	if (num_registered_fb > 1)
 		fb_blank(registered_fb[1], FB_BLANK_NORMAL);
-#ifdef CONFIG_TEGRA_CONVSERVATIVE_GOV_ON_EARLYSUPSEND
-	cpufreq_save_default_governor();
-	cpufreq_set_conservative_governor();
-	cpufreq_set_conservative_governor_param(
-		SET_CONSERVATIVE_GOVERNOR_UP_THRESHOLD,
-		SET_CONSERVATIVE_GOVERNOR_DOWN_THRESHOLD);
-#endif
 }
 
 static void cardhu_panel_late_resume(struct early_suspend *h)
 {
 	unsigned i;
-#ifdef CONFIG_TEGRA_CONVSERVATIVE_GOV_ON_EARLYSUPSEND
-	cpufreq_restore_default_governor();
-#endif
 	for (i = 0; i < num_registered_fb; i++)
 		fb_blank(registered_fb[i], FB_BLANK_UNBLANK);
 }
