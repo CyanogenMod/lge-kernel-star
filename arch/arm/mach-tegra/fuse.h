9ec9491dad357e3a48c556bc96357a4cb9474d62
Diwakar Tundlam <dtundlam@nvidia.com>
2011-12-05 14:09:59 -0800

    arm: tegra: Fix Tegra2/Tegra3 conditionals for speedo APIs
    Change-Id: Ia97169c6bcc84dac03439ff9feb9e8b37763fa09
    Signed-off-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Reviewed-on: http://git-master/r/68284
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>

--- arch/arm/mach-tegra/fuse.h
+++ arch/arm/mach-tegra/fuse.h
@@ -34,16 +34,19 @@ const char *tegra_get_revision_name(void);
 
 int tegra_cpu_process_id(void);
 int tegra_core_process_id(void);
-int tegra_cpu_speedo_id(void);
 int tegra_soc_speedo_id(void);
-int tegra_cpu_speedo_mv(void);
-int tegra_core_speedo_mv(void);
 void tegra_init_speedo_data(void);
 
 #ifndef CONFIG_ARCH_TEGRA_2x_SOC
 int tegra_package_id(void);
+int tegra_cpu_speedo_id(void);
+int tegra_cpu_speedo_mv(void);
+int tegra_core_speedo_mv(void);
 #else
 static inline int tegra_package_id(void) { return -1; }
+static inline int tegra_cpu_speedo_id(void) { return 0; }
+static inline int tegra_cpu_speedo_mv(void) { return 1000; }
+static inline int tegra_core_speedo_mv(void) { return 1200; }
 #endif
 
 #else
@@ -52,6 +55,7 @@ static inline int tegra_cpu_process_id(void) { return 0; }
 static inline int tegra_core_process_id(void) { return 0; }
 static inline int tegra_cpu_speedo_id(void) { return 0; }
 static inline int tegra_soc_speedo_id(void) { return 0; }
+static inline int tegra_package_id(void) { return -1; }
 static inline int tegra_cpu_speedo_mv(void) { return 1000; }
 static inline int tegra_core_speedo_mv(void) { return 1200; }
 static inline void tegra_init_speedo_data(void) { }
