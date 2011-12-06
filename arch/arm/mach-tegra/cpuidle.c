5d672c89ce2041e032febf4a763ec9d1bb1aad26
Prashant Gaikwad <pgaikwad@nvidia.com>
2011-12-06 17:27:44 +0530

    Revert "ARM: tegra: power: Use peek ahead timers not event notification"
    This reverts commit 3f5e640849383b1d5ed551aa84e0a89578519f09.
    
    Change-Id: I5af3fab193204d121a0ea4a939d6e950d2c4a633
    Reviewed-on: http://git-master/r/68680
    Reviewed-by: Andy Park <andyp@nvidia.com>
    Tested-by: Andy Park <andyp@nvidia.com>
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>

--- arch/arm/mach-tegra/cpuidle.c
+++ arch/arm/mach-tegra/cpuidle.c
@@ -108,6 +108,7 @@ static int tegra_idle_enter_lp2(struct cpuidle_device *dev,
 		return tegra_idle_enter_lp3(dev, state);
 
 	local_irq_disable();
+	clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_ENTER, &dev->cpu);
 	enter = ktime_get();
 
 	tegra_cpu_idle_stats_lp2_ready(dev->cpu);
@@ -116,10 +117,8 @@ static int tegra_idle_enter_lp2(struct cpuidle_device *dev,
 	exit = ktime_sub(ktime_get(), enter);
 	us = ktime_to_us(exit);
 
+	clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_EXIT, &dev->cpu);
 	local_irq_enable();
-
-	/* cpu clockevents may have been reset by powerdown */
-	hrtimer_peek_ahead_timers();
 
 	smp_rmb();
 
