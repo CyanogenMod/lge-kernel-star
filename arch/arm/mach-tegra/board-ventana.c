bf69e858ce3ec1d68f1b59dc64f7b4c11aadcd34
Shashank Sharma <shashanks@nvidia.com>
2011-12-16 18:10:46 +0530

    arm: tegra: display: Increased FB size
    Added 1MB extra memory for the first FB device, required for double buffering.
    
    Bug 898400
    
    Change-Id: I92245fdb2e7a1d71888fc0831f052017608b975a
    Signed-off-by: Shashank Sharma (shashanks@nvidia.com)
    Signed-off-by: Jong Kim <jongk@nvidia.com>
    Reviewed-on: http://git-master/r/69838
    Reviewed-on: http://git-master/r/70946
    Tested-by: Shashank Sharma <shashanks@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Allen Martin <amartin@nvidia.com>

--- arch/arm/mach-tegra/board-ventana.c
+++ arch/arm/mach-tegra/board-ventana.c
@@ -639,7 +639,7 @@ void __init tegra_ventana_reserve(void)
 	if (memblock_reserve(0x0, 4096) < 0)
 		pr_warn("Cannot reserve first 4K of memory for safety\n");
 
-	tegra_reserve(SZ_256M, SZ_8M, SZ_16M);
+	tegra_reserve(SZ_256M, SZ_8M + SZ_1M, SZ_16M);
 }
 
 MACHINE_START(VENTANA, "ventana")
